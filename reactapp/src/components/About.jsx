23678dcb370a83eb9325574bfa37b150b0e6e557
Kevin Tran <kevtran04@gmail.com>
2024-12-27 21:29:03 -0600

    complete

--- reactapp/src/components/About.jsx
+++ reactapp/src/components/About.jsx
@@ -1,9 +1,7 @@
 import aboutImage from "../assets/about.jpg";
 import chicagoCafe from "../assets/ChiCafe.jpg";
 import { FaArrowLeft } from 'react-icons/fa';
-import { ABOUT_TEXT } from "../constants";
-import { ABOUT_TEXT2 } from "../constants";
-import { ABOUT_TEXT3 } from "../constants";
+import { ABOUT_TEXT, ABOUT_TEXT2, ABOUT_TEXT3 } from "../constants";
 import { motion } from "framer-motion";
 
 const About = () => {
@@ -19,7 +17,7 @@ return (
             transition={{ duration: 0.5 }}
             className="w-full lg:w-1/2 lg:p-8 flex items-start">
                 <div 
-                className="flex items-center justify-center space-x-4">
+                className="flex items-center justify-center space-x-4 ml-20">
                     <img src={aboutImage} alt="about" className="rounded-2xl w-24 h-auto lg:w-96"/>
                     <img src={chicagoCafe} alt="about" className="rounded-2xl w-24 h-auto lg:w-96"/>
                     
@@ -29,8 +27,8 @@ return (
             whileInView={{ opacity: 1, x: 0 }}
             initial={{ opacity: 0, x: 100}}
             transition={{duration: 0.5}}
-            className="w-full lg:w-1/2 flex items-start mt-8 lg:mt-6 pr-4">
-                <div className="flex flex-col justify-center lg:justify-start space-y-4">
+            className="w-full lg:w-1/2 flex items-start mt-8 lg:mt-6 pr-4 lg:pl-8">
+                <div className="flex flex-col justify-center lg:justify-start space-y-4 lg:ml-16">
                     <p className="max-w-xl">{ABOUT_TEXT}</p>
                     <p className="max-w-xl">{ABOUT_TEXT2}</p>
                     <p className="max-w-xl">{ABOUT_TEXT3}</p>
--- reactapp/src/components/Contact.jsx
+++ reactapp/src/components/Contact.jsx
@@ -1,14 +1,27 @@
 import { CONTACT } from "../constants/index";
 import { FaRegCopyright } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Contact = () => {
 return (
     <div className="border-b border-neutral-900 pb-20 flex justify-center items-center">
         <div className="text-center">
-            <h2 className="my-10 text-4xl">Contact Me!</h2>
+            <motion.h2 
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5 }}
+            className="my-10 text-4xl">Contact Me!</motion.h2>
             <div className="tracking-tighter">
-                <p className="my-4">{CONTACT.address}</p>
-                <p className="my-4">{CONTACT.email}</p>
+                <motion.p 
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+                className="my-4">{CONTACT.address}</motion.p>
+                <motion.p
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 1 }} 
+                className="my-4">{CONTACT.email}</motion.p>
                 <a href="https://www.linkedin.com/in/kevintran77" className="border-b">
                     {CONTACT.linked_in}
                 </a>
