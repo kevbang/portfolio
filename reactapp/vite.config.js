eb11a6076d4a1b86bde1d34ed9af4211b429d630
Kevin Tran <kevtran04@gmail.com>
2024-12-27 22:32:58 -0600

    Update vite.config.js

--- reactapp/vite.config.js
+++ reactapp/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.JPG"]
+  assetsInclude: ["**/*.JPG"],
+  build: {
+    outDir: "dist"
+  }
 })
