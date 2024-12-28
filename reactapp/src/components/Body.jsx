ef7c9582c120b4c898b2e3c44cef145d24b5dc4e
Kevin Tran <kevtran04@gmail.com>
2024-12-27 20:27:20 -0600

    animate

--- reactapp/src/components/Body.jsx
+++ reactapp/src/components/Body.jsx
@@ -3,28 +3,43 @@ import profilePic from "../assets/kevinPic.jpeg";
 import johnDeereLogo from "../assets/johnDeereLogo.png";
 import { motion } from "framer-motion";
 
+const container = (delay) => ({
+  hidden: {x: -100, opacity: 0},
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {duration: 0.5, delay: delay},
+  },
+});
+
 const Body = () => {
   return (
     <div className="container mx-auto px-4">
       <div className="flex flex-wrap items-center">
         <div className="w-full lg:w-1/2 lg:pr-4"> {/* Changed padding to right */}
           <div className="text-center lg:text-left">
-            <motion.h1 
-            initial={{x: -100, opacity: 0}} 
-            animate={{x:0, opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.5 }}
+            <motion.h1
+            variants={container(0)}
+            initial="hidden"
+            animate="visible"
             className="pb-12 font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Kevin Tran
             </motion.h1>
-            <motion.span whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.1 }} drag="x" dragConstraints={{ left: -100, right: 100 }} className="bg-gradient-to-r from-red-600 via-red-400 to-yellow-400 bg-clip-text text-4xl tracking-tight text-transparent"> Student Software Engineer</motion.span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            <motion.span 
+            variants={container(0.5)}
+            initial="hidden"
+            animate="visible" className="bg-gradient-to-r from-red-600 via-red-400 to-yellow-400 bg-clip-text text-4xl tracking-tight text-transparent">Student Software Engineer</motion.span>
+            <motion.p
+            variants={container(1)}
+            initial="hidden"
+            animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">
               {BODY_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:pl-8"> {/* Changed padding to left */}
           <div className="flex flex-col justify-center lg:justify-end space-y-4"> {/* Changed to flex-col and space-y-4 for vertical spacing */}
-            <img src={profilePic} alt="Kevin Tran" className="w-80 h-130" />
+            <motion.img initial={{ x:100, opacity:0 }} animate={{ x:0, opacity:1 }} transition={{duration: 1, delay: 1.2}} src={profilePic} alt="Kevin Tran" className="w-80 h-130" />
           </div>
         </div>
       </div>
--- reactapp/src/components/Technologies.jsx
+++ reactapp/src/components/Technologies.jsx
@@ -4,35 +4,79 @@ import { BiLogoSpringBoot } from "react-icons/bi";
 import { FaJava } from "react-icons/fa";
 import { FaPython } from "react-icons/fa";
 import { SiTerraform } from "react-icons/si";
+import { motion } from "framer-motion";
 
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration, 
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse"
+        }
+    }
+})
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h2 className="my-20 text-center text-4xl">
+        <motion.h2 
+        whileInView={{opacity: 1, y:0}}
+        initial={{ opacity:0, y: -100 }}
+        transition={{duration: 1.5}}
+        className="my-20 text-center text-4xl">
             Shenanigans
-        </h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 py-4">
+        </motion.h2>
+        <motion.div 
+        whileInView={{opacity: 1, x:0}}
+        initial={{opacity:0, x:-100}}
+        transition={{duration:1.5}}
+        className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 py-4">
                 <RiGithubFill className="text-7xl text-white-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 py-4">
+            </motion.div>
+            <motion.div
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 py-4">
                 <BiLogoSpringBoot className="text-7xl text-green-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 py-4">
+            </motion.div>
+            <motion.div
+            variants={iconVariants(5)}
+            initial="initial"
+            animate="animate" 
+            className="rounded-2xl border-4 border-neutral-800 py-4">
                 <FaJava className="text-7xl text-blue-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 py-4">
+            </motion.div>
+            <motion.div
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 py-4">
                 <FaPython className="text-7xl text-yellow-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 py-4">
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 py-4">
                 <RiDatabase2Fill className="text-7xl text-red-600"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 py-4">
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(3.5)}
+            initial="initial"
+            animate="animate"className="rounded-2xl border-4 border-neutral-800 py-4">
                 <SiTerraform className="text-7xl text-purple-500"/>
-            </div>
+            </motion.div>
             
-        </div>
+        </motion.div>
         </div>
   )
 }
