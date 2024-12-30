a558ba02bd3058661d22eb0a2fce914d80641b57
Kevin Tran <kevtran04@gmail.com>
2024-12-30 11:35:47 -0600

    change bundle & (change blueprint)

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_lightsail_instance" "kevsite" {
   name              = "kevsite-instance"
   availability_zone = "us-east-1b"
   blueprint_id      = "nodejs"
-  bundle_id         = "nano_3_0"
+  bundle_id         = "micro_3_0"
   add_on {
     type          = "AutoSnapshot"
     snapshot_time = "06:00"
